0228117c359fb0825f852d1c66ec2a53f7b96a2c
Afaan Bilal <afaanbilal@gmail.com>
2023-04-12 16:30:26 +0530

    📱 Responsive orgs

--- src/components/Orgs.tsx
+++ src/components/Orgs.tsx
@@ -17,14 +17,14 @@ const Orgs: React.FC<OrgsProps> = ({ orgs }) => {
     return (
         <div className="p-2 border-b border-gray-700">
             <div className="text-2xl text-gray-500 m-2">Orgs ({orgs.length})</div>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap items-center justify-center">
                 {orgs.map(o =>
                     <a key={o.login} href={"https://github.com/" + o.login} target="_blank" rel="noopener noreferrer">
-                        <div className="flex flex-col w-48 m-2 p-4 items-center border border-gray-700 rounded-lg hover:border-cyan-600">
+                        <div className="flex flex-col w-40 m-2 p-4 items-center border border-gray-700 rounded-lg hover:border-cyan-600">
                             <div className="my-2">
                                 <img src={o.avatar_url} className="w-16 rounded-xl" alt="avatar" />
                             </div>
-                            <div className="text-sm mt-2">{o.login}</div>
+                            <div className="text-xs mt-2">{o.login}</div>
                         </div>
                     </a>
                 )}
